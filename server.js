89fa48142008511376c7ffd68693abbcb03847e8
Caleb O'Leary <CO'Leary@merchant.local>
2016-06-08 12:32:07 -0700

    heroku sleep fix

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 var Botkit = require('botkit');
 var fs = require('fs');
 
+// fixing heroku sleep problem
+	var handle, http, request, server, strobe;
+	http = require('http');
+	request = require('request');
+	strobe = function() {
+	  return request('http://sites-using-bot.herokuapp.com/', function(e, r, b) {});
+	};
+	(function() {
+	  setInterval(strobe, 25 * 60 * 1000);
+	  return console.log("I am up!!");
+	})();
+	handle = function(req, res) {
+	  return res.end("42");
+	};
+	server = http.createServer(handle);
+	server.listen(process.env.PORT || 5000);
+// fixing heroku sleep problem
+
 var controller = Botkit.slackbot({
 	debug: true
 });
